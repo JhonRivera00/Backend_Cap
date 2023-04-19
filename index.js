1176d0e251ff9d022210ce74e740aa4a18e19803
JhonRivera00 <jarivera808@misena.edu.co>
2023-04-19 10:00:41 -0500

    add response app

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import app from './src/app.js';
 import { PORT } from './src/config.js';
 import './src/db.js';
 
+app.use((req, res, next)=>{
+    return res.status(405).json("Cap proyecto")
+})
+
 app.listen(PORT, ()=>{
     console.log(`Servidor en puerto ${PORT}`);
 });
